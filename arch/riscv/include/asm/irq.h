a3f526153fa9c6299e4e30b12e43ecf05f879294
Vladimir Kondratiev <vladimir.kondratiev@mobileye.com>
2025-06-12 17:39:05 +0300

    UPSTREAM: riscv: Helper to parse hart index
    RISC-V APLIC specification defines "hart index" in [1]. Similar definitions
    can be found for ACLINT in [2]
    
    Quote from the APLIC specification:
    
    Within a given interrupt domain, each of the domain’s harts has a unique
    index number in the range 0 to 2^14 − 1 (= 16,383). The index number a
    domain associates with a hart may or may not have any relationship to the
    unique hart identifier (“hart ID”) that the RISC-V Privileged
    Architecture assigns to the hart. Two different interrupt domains may
    employ entirely different index numbers for the same set of harts.
    
    Further, it says in "4.5 Memory-mapped control region for an interrupt
    domain":
    
    The array of IDC structures may include some for potential hart index
    numbers that are not actual hart index numbers in the domain.  For example,
    the first IDC structure is always for hart index 0, but 0 is not
    necessarily a valid index number for any hart in the domain.
    
    Support arbitrary hart indices specified in an optional property
    "riscv,hart-indexes" which is specified as an array of u32 elements, one
    per interrupt target, listing hart indexes in the same order as in
    "interrupts-extended".
    
    If this property is not specified, fall back to use logical hart indices
    within the domain.
    
    Signed-off-by: Vladimir Kondratiev <vladimir.kondratiev@mobileye.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/all/20250612143911.3224046-2-vladimir.kondratiev@mobileye.com
    Link: https://github.com/riscv/riscv-aia [1]
    Link: https://github.com/riscvarchive/riscv-aclint [2]
    
    (cherry picked from commit 5fe331cdcfba5b2c2dd8211127dadefcaaa97dec)
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- arch/riscv/include/asm/irq.h
+++ arch/riscv/include/asm/irq.h
@@ -15,5 +15,7 @@
 void riscv_set_intc_hwnode_fn(struct fwnode_handle *(*fn)(void));
 
 struct fwnode_handle *riscv_get_intc_hwnode(void);
+int riscv_get_hart_index(struct fwnode_handle *fwnode, u32 logical_index,
+			 u32 *hart_index);
 
 #endif /* _ASM_RISCV_IRQ_H */
