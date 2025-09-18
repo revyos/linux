9f548b9b95292b7cdc70cde07781fe9c3a7991ad
Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
2025-09-18 13:58:56 -0700

    FROMLIST: PCI: Release BAR0 of an integrated bridge to allow GPU BAR resize
    Resizing BAR to a larger size has to release upstream bridge windows in
    order make the bridge windows larger as well (and to potential relocate
    them into a larger free block within iomem space). Some GPUs have an
    integrated PCI switch that has BAR0. The resource allocation assigns
    space for that BAR0 as it does for any resource.
    
    An extra resource on a bridge will pin its upstream bridge window in
    place which prevents BAR resize for anything beneath that bridge.
    
    Nothing in the pcieport driver provided by PCI core, which typically is
    the driver bound to these bridges, requires that BAR0. Because of that,
    releasing the extra BAR does not seem to have notable downsides but
    comes with a clear upside.
    
    Therefore, release BAR0 of such switches using a quirk and clear its
    flags to prevent any new invocation of the resource assignment
    algorithm from assigning the resource again.
    
    Due to other siblings within the PCI hierarchy of all the devices
    integrated into the GPU, some other devices may still have to be
    manually removed before the resize is free of any bridge window pins.
    Such siblings can be released through sysfs to unpin windows while
    leaving access to GPU's sysfs entries required for initiating the
    resize operation, whereas removing the topmost bridge this quirk
    targets would result in removing the GPU device as well so no manual
    workaround for this problem exists.
    
    Reported-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Link: https://lore.kernel.org/linux-pci/fl6tx5ztvttg7txmz2ps7oyd745wg3lwcp3h7esmvnyg26n44y@owo2ojiu2mov/
    Link: https://lore.kernel.org/intel-xe/20250721173057.867829-1-uwu@icenowy.me/
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Cc: stable@vger.kernel.org # v6.12+
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Link: https://lore.kernel.org/intel-xe/fafda2a3-fc63-ce97-d22b-803f771a4d19@linux.intel.com
    Link: https://lore.kernel.org/r/20250918-xe-pci-rebar-2-v1-1-6c094702a074@intel.com
    Signed-off-by: Han Gao <rabenda.cn@gmail.com>

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -6378,3 +6378,26 @@ static void quirk_sg2042_no_port_services(struct pci_dev *dev)
 	dev->dev_flags |= PCI_DEV_FLAGS_NO_PORT_SERVICES;
 }
 DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_SOPHGO, 0x2042, quirk_sg2042_no_port_services);
+
+/*
+ * PCI switches integrated into Intel Arc GPUs have BAR0 that prevents
+ * resizing the BARs of the GPU device due to that bridge BAR0 pinning the
+ * bridge window it's under in place. Nothing in pcieport requires that
+ * BAR0.
+ *
+ * Release and disable BAR0 permanently by clearing its flags to prevent
+ * anything from assigning it again.
+ */
+static void pci_release_bar0(struct pci_dev *pdev)
+{
+	struct resource *res = pci_resource_n(pdev, 0);
+
+	if (!res->parent)
+		return;
+
+	pci_release_resource(pdev, 0);
+	res->flags = 0;
+}
+DECLARE_PCI_FIXUP_ENABLE(PCI_VENDOR_ID_INTEL, 0x4fa0, pci_release_bar0);
+DECLARE_PCI_FIXUP_ENABLE(PCI_VENDOR_ID_INTEL, 0x4fa1, pci_release_bar0);
+DECLARE_PCI_FIXUP_ENABLE(PCI_VENDOR_ID_INTEL, 0xe2ff, pci_release_bar0);
