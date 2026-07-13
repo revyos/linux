d1f7567ff52993dccb1373ddd12f7cb5892987d5
Zhanpeng Zhang <zhangzhanpeng.jasper@bytedance.com>
2026-07-13 20:29:03 +0800

    FROMLIST: iommu/riscv: Use 32-bit MMIO accesses for 64-bit registers
    The RISC-V IOMMU specification [1] permits 64-bit registers to be accessed
    using two 32-bit transactions, high half first, and leaves the single-copy
    atomicity of 8-byte IOMMU register accesses unspecified.
    
    Use the generic hi_lo_readq_relaxed() and hi_lo_writeq_relaxed() helpers
    for ordinary 64-bit IOMMU registers. For DDTP, poll BUSY in the low half,
    then read the high half and compose the register value from the polled low
    half. HPM counter reads require a rollover-aware sequence and remain
    outside these accessors.
    
    This follows the 32-bit access direction proposed by Guo Ren [2] and uses
    the generic non-atomic MMIO helpers suggested by David Laight.
    
    [1] https://docs.riscv.org/reference/iommu/
    [2] https://lore.kernel.org/r/20250903144217.837448-1-guoren@kernel.org
    
    Suggested-by: Guo Ren <guoren@kernel.org>
    Suggested-by: David Laight <david.laight.linux@gmail.com>
    Signed-off-by: Zhanpeng Zhang <zhangzhanpeng.jasper@bytedance.com>
    Reviewed-by: Guo Ren (Alibaba DAMO Academy) <guoren@kernel.org>
    Co-developed-by: Guo Ren (Alibaba DAMO Academy) <guoren@kernel.org>
    Signed-off-by: Guo Ren (Alibaba DAMO Academy) <guoren@kernel.org>
    Acked-by: Chen Pei <cp0613@linux.alibaba.com>
    Reviewed-by: Tomasz Jeznach <tomasz.jeznach@linux.dev>
    Link: https://lore.kernel.org/r/20260713122903.9458-1-zhangzhanpeng.jasper@bytedance.com
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- drivers/iommu/riscv/iommu.c
+++ drivers/iommu/riscv/iommu.c
@@ -692,9 +692,12 @@ void riscv_iommu_disable(struct riscv_iommu_device *iommu)
 
 #define riscv_iommu_read_ddtp(iommu) ({ \
 	u64 ddtp; \
-	riscv_iommu_readq_timeout((iommu), RISCV_IOMMU_REG_DDTP, ddtp, \
-				  !(ddtp & RISCV_IOMMU_DDTP_BUSY), 10, \
+	u32 ddtp_lo, ddtp_hi; \
+	riscv_iommu_readl_timeout((iommu), RISCV_IOMMU_REG_DDTP, ddtp_lo, \
+				  !(ddtp_lo & RISCV_IOMMU_DDTP_BUSY), 10, \
 				  RISCV_IOMMU_DDTP_TIMEOUT); \
+	ddtp_hi = riscv_iommu_readl((iommu), RISCV_IOMMU_REG_DDTP + 4); \
+	ddtp = ((u64)ddtp_hi << 32) | ddtp_lo; \
 	ddtp; })
 
 static int riscv_iommu_iodir_alloc(struct riscv_iommu_device *iommu)
@@ -1523,7 +1526,7 @@ static int riscv_iommu_init_check(struct riscv_iommu_device *iommu)
 	 * regular boot flow and disable translation when we boot into a kexec
 	 * kernel and the previous kernel left them enabled.
 	 */
-	ddtp = riscv_iommu_readq(iommu, RISCV_IOMMU_REG_DDTP);
+	ddtp = riscv_iommu_read_ddtp(iommu);
 	if (ddtp & RISCV_IOMMU_DDTP_BUSY)
 		return -EBUSY;
 
