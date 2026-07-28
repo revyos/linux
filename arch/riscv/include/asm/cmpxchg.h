d551a74d9592f346d31acef1093365d015cc9ff6
Yicong Yang <yang.yicong@picoheart.com>
2026-07-28 17:46:27 +0800

    FROMLIST: riscv: errata: picoheart: Add workaround for AMOCASQ errata
    Some Picoheart CPUs implement Zacas extension but lack support
    for AMOCASQ PMA attribute. Thus makes the CASQ instruction de
    facto unavailable. Since currently no ways to retrieve the PMA
    information and the riscv kernel will declare cmpxchg128 support
    if platform declare support of Zacas, the use of cmpxchg128 will
    lead to PMA violation and crash the kernel. Add the errata
    workaround to disable the use of cmpxchg128 on the affected CPUs.
    
    Signed-off-by: Yicong Yang <yang.yicong@picoheart.com>
    Link: https://lore.kernel.org/r/20260728094627.94975-7-yang.yicong@picoheart.com
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- arch/riscv/include/asm/cmpxchg.h
+++ arch/riscv/include/asm/cmpxchg.h
@@ -329,7 +329,7 @@
 
 #if defined(CONFIG_64BIT) && defined(CONFIG_RISCV_ISA_ZACAS) && defined(CONFIG_TOOLCHAIN_HAS_ZACAS)
 
-#define system_has_cmpxchg128()        riscv_has_extension_unlikely(RISCV_ISA_EXT_ZACAS)
+#define system_has_cmpxchg128	system_has_cmpxchg128
 
 union __u128_halves {
 	u128 full;
