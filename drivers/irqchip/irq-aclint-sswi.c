83b310fe9c76420634a177edcbe1c409a983e45a
Junhui Liu <junhui.liu@pigmoral.tech>
2026-09-18 00:29:37 +0800

    FROMLIST: irqchip/aclint-sswi: Add support for SpacemiT K3
    Add a match entry for the spacemit,k3-aclint-sswi compatible. The K3
    SSWI device requires no special handling and can use the existing
    generic ACLINT SSWI initialization function.
    
    Signed-off-by: Junhui Liu <junhui.liu@pigmoral.tech>
    Link: https://lore.kernel.org/r/20260918-k3-aclint-v3-5-cf4fd4050e14@pigmoral.tech
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- drivers/irqchip/irq-aclint-sswi.c
+++ drivers/irqchip/irq-aclint-sswi.c
@@ -179,6 +179,7 @@ static int __init generic_aclint_sswi_early_probe(struct device_node *node,
 }
 IRQCHIP_DECLARE(mips_p8700_sswi, "mips,p8700-aclint-sswi", generic_aclint_sswi_early_probe);
 IRQCHIP_DECLARE(nuclei_ux900_sswi, "nuclei,ux900-aclint-sswi", generic_aclint_sswi_early_probe);
+IRQCHIP_DECLARE(spacemit_k3_sswi, "spacemit,k3-aclint-sswi", generic_aclint_sswi_early_probe);
 
 /* THEAD variant */
 #define THEAD_C9XX_CSR_SXSTATUS			0x5c0
